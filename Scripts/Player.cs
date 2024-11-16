a31f7b6a95b798e43045118306d4ed1ee5eb7dcf
AidanHart0602 <aidanhart321@gmail.com>
2024-11-15 22:24:23 -1000

    Movement Clean up

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,54 +4,77 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float _speed = 3;
-    [SerializeField]
-    private float _jumpPower = 1;
-    [SerializeField]
-    private bool _grounded = false;
+    [SerializeField] private float _speed = 3;
+    [SerializeField] private float _jumpPower = 1;
+    [SerializeField] private bool _grounded = false;
     private bool _jumpCD = false;
     private Rigidbody2D _rb;
-
-    private SpriteRenderer _playerSprite;
+    [SerializeField] private Animator _playerAnim;
+    [SerializeField] private SpriteRenderer _playerSprite;
     // Start is called before the first frame update
     void Start()
-    {
-        _playerSprite = GetComponent<SpriteRenderer>();
+    { 
         _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        Grounded();
+    }
+    void Movement()
+    {
         float horizontal = Input.GetAxis("Horizontal");
-
         _rb.velocity = new Vector2(horizontal * _speed, _rb.velocity.y);
+
         if (_grounded == true && Input.GetKeyDown(KeyCode.Space))
         {
             _rb.velocity = new Vector2(_rb.velocity.x, _jumpPower);
             _grounded = false;
-            _jumpCD = true;
             StartCoroutine(JumpCooldown());
         }
+        AnimationChecks(horizontal);
+    }
 
+    void AnimationChecks(float Direction)
+    {
+        if (Direction > 0)
+        {
+            _playerSprite.flipX = false;
+            _playerAnim.SetBool("Running", true);
+        }
+
+        else if (Direction < 0)
+        {
+            _playerSprite.flipX = true;
+            _playerAnim.SetBool("Running", true);
+        }
+
+        else if(Direction == 0)
+        {
+            _playerAnim.SetBool("Running", false);
+        }
+    }
+
+    private void Grounded()
+    {
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, .6f, 1 << 8);
         Debug.DrawRay(transform.position, Vector2.down, Color.green, .6f);
 
-        if (hitInfo != null)
+        if (hitInfo.collider != null)
         {
+            Debug.Log(hitInfo.collider);
             if (_jumpCD == false)
             {
                 _grounded = true;
             }
         }
-
-
     }
-
     IEnumerator JumpCooldown()
     {
-        yield return new WaitForSeconds(.1f);
+        _jumpCD = true;
+        yield return new WaitForSeconds(.2f);
         _jumpCD = false;
     }
 }
